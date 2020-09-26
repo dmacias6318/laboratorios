1ae3f40330adee9cfb0b1ac7d32e87bad8cc86eb
monkyo97 <44757849+monkyo97@users.noreply.github.com>
2020-09-25 23:42:56 -0500

    xxxx

--- src/pgsql.js
+++ src/pgsql.js
@@ -2,8 +2,9 @@ const { Pool } = require('pg')
 
 const data = {
     user: 'postgres',
-    host: 'ip del equipo',
-    password: 'password del gestor sql',
+    host: '34.68.255.88',
+    //host: '127.0.0.1',
+    password: 'Sys25992.*',
     database: 'lab',
     port: 5432
 };
